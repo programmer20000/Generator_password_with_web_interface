94789f9ecb49477261a0af3738bff29c8e6dbfc4
Mihai Apostol <mihaiapostol153@gmail.com>
2022-12-27 17:14:25 +0200

    add other files proojec

---
+++ main.py
@@ -0,0 +1,41 @@
+import os
+from random import randint
+import pywebio
+from pywebio import start_server
+from pywebio.output import (put_image, put_html, put_button, put_success, put_error)
+from pywebio.input import (input, NUMBER)
+from pyperclip import copy
+from symbols_for_password import symbols_for_password
+
+pywebio.config(title='Generator Password')
+pywebio.config(theme='dark')
+
+password = ''
+
+
+def next_interface():
+    put_html(f'<h1>Your Password is : {password} </h1>')
+    put_success('Generating password is successful',closable=True)
+    put_button(label='copy password', onclick=lambda: 'Hello World')
+
+
+def generator_password():
+    length_password_input = input(placeholder='Please Enter length password', type=NUMBER, maxlength=2048)
+    global password
+    for i in range(length_password_input):
+        password = f'{password}{symbols_for_password[randint(0, len(symbols_for_password) - 1)]}'
+
+    next_interface()
+
+    if length_password_input == type(str):
+        put_error('You inserted symbols, but must be entering integer number from 1 to 2048')
+
+
+def main():
+    logo_path = os.path.join('image', 'logo.jpg')
+    put_image(open(file=logo_path, mode='rb').read())
+    generator_password()
+
+
+if __name__ == '__main__':
+    start_server(main, port=8000, debug=True)
