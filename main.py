194f1cf9008c14efcf351f09eac8c6ddda37d80e
Mihai Apostol <mihaiapostol153@gmail.com>
2022-12-28 14:06:41 +0200

    add function refresh_page

--- main.py
+++ main.py
@@ -2,39 +2,45 @@ import os
 from random import randint
 import pywebio
 from pywebio import start_server
-from pywebio.output import (put_image, put_html, put_button, put_success, put_error)
+from pywebio.output import (put_image, put_html, put_button, put_success, put_error, clear)
 from pywebio.input import (input, NUMBER)
 from pyperclip import copy
-from symbols_for_password import symbols_for_password
+from symbols_for_password.symbols_for_password import symbols_for_password
 
 pywebio.config(title='Generator Password')
 pywebio.config(theme='dark')
 
 password = ''
 
+def refresh_page():
+   main()
 
 def next_interface():
     put_html(f'<h1>Your Password is : {password} </h1>')
-    put_success('Generating password is successful',closable=True)
-    put_button(label='copy password', onclick=lambda: 'Hello World')
+    put_success('Generating password is successful', closable=True)
+    put_button(label='copy password', onclick=lambda: copy(password))
+    put_button(label='refresh page', onclick=refresh_page)
+
 
 
 def generator_password():
-    length_password_input = input(placeholder='Please Enter length password', type=NUMBER, maxlength=2048)
-    global password
-    for i in range(length_password_input):
-        password = f'{password}{symbols_for_password[randint(0, len(symbols_for_password) - 1)]}'
+   try:
+       length_password_input = input(placeholder='Please Enter length password', type=NUMBER, maxlength=2048)
+       global password
+       for i in range(length_password_input):
+           password = f'{password}{symbols_for_password[randint(0, len(symbols_for_password) - 1)]}'
+       next_interface()
 
-    next_interface()
-
-    if length_password_input == type(str):
-        put_error('You inserted symbols, but must be entering integer number from 1 to 2048')
-
+   except Exception as ex:
+       put_error('Your password is very big',closable=True)
+       generator_password()
 
 def main():
+    clear()
     logo_path = os.path.join('image', 'logo.jpg')
     put_image(open(file=logo_path, mode='rb').read())
     generator_password()
+
 
 
 if __name__ == '__main__':
