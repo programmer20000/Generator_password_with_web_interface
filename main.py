b7dcc8e6a9020c34c3d061745cd73bb0412cd716
Mihai Apostol <mihaiapostol153@gmail.com>
2023-02-24 17:01:01 +0200

    Rewritten program with using OOP

--- main.py
+++ main.py
@@ -6,51 +6,57 @@ from pyperclip import copy
 from pywebio import start_server
 from pywebio.input import (input, NUMBER)
 from pywebio.output import (put_image, put_html, put_button, put_success, put_error, put_row, clear)
+from pywebio.session import download
 
 from symbols_for_password.symbols_for_password import symbols_for_password
 
-pywebio.config(title='Generator Password')
-pywebio.config(theme='dark')
 
-password = ''
+class GeneratorPassword:
+    def __init__(self):
 
+        # todo: here we setup  window with title and theme properties
+        pywebio.config(title='Generator Paasword', theme='dark')
 
-def refresh_page():
-    # todo: This function is for refresh page by press button
-    main()
+        self.password = ''
 
+    def refresh_page(self):
+        # todo: This function is for refresh page by press button
+        self.run()
 
-def next_interface():
-    put_html(f'<h1>Your Password is : {password} </h1>')
-    put_success('Generating password is successful', closable=True)
+    def next_interface(self):
+        put_html(f'<h1>Your Password is : {self.password} </h1>')
+        put_success('Generating password is successful', closable=True)
 
-    put_row([put_button(label='copy password', onclick=lambda: copy(password)), None,
-             put_button(label='refresh page', onclick=refresh_page), put_button(label='write in file', onclick='')],
-            size='16% 5px 16%')
+        put_row([put_button(label='copy password', onclick=lambda: copy(self.password)), None,
+                 put_button(label='refresh page', onclick=self.refresh_page),
+                 put_button(label='write in file',
+                            onclick=[lambda: download(name='download file', content=self.password)])],
+                size='16% 5px 16%')
 
+    def generator_password(self):
+        # todo: This function is for generate password
+        self.placeholder = 'Please Enter length password'
+        self.help_text = 'Max count of the symbols, your password not has to be more big of area offered (1-2048)'
 
+        try:
+            length_password_input = input(placeholder=self.placeholder, help_text=self.help_text, type=NUMBER,
+                                          maxlength=2048, required=True)
 
-def generator_password():
-    # todo: This function is for generate password
+            for i in range(length_password_input):
+                self.password = f'{self.password}{symbols_for_password[randint(0, len(symbols_for_password) - 1)]}'
+            self.next_interface()
 
-    try:
-        length_password_input = input(placeholder='Please Enter length password', type=NUMBER, maxlength=2048)
-        global password
-        for i in range(length_password_input):
-            password = f'{password}{symbols_for_password[randint(0, len(symbols_for_password) - 1)]}'
-        next_interface()
+        except Exception as ex:
+            put_error('Your password is very big', closable=True)
+            self.generator_password()
 
-    except Exception as ex:
-        put_error('Your password is very big', closable=True)
-        generator_password()
-
-
-def main():
-    clear()
-    logo_path = os.path.join('image', 'logo.jpg')
-    put_image(open(file=logo_path, mode='rb').read())
-    generator_password()
+    def run(self):
+        clear()
+        logo_path = os.path.join('image', 'logo.jpg')
+        put_image(open(file=logo_path, mode='rb').read())
+        self.generator_password()
 
 
 if __name__ == '__main__':
-    start_server(main, port=8000, debug=True)
+    generator = GeneratorPassword()
+    start_server(generator.run, port=8000, debug=True)
